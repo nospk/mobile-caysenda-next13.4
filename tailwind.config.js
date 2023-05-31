8e00afc1aed9327b19a22109f77bdd38ae14fb17
nospk <nospk1706@gmail.com>
2023-05-31 10:04:12 +0000

    update animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-google)'],
+        sans: ["var(--font-google)"],
       },
-      maxWidth:{
-        '40px':'40px',
-        '60px':'60px'
+      maxWidth: {
+        "40px": "40px",
+        "60px": "60px",
       },
-      width:{
-        '46vw':'46.4vw'
+      width: {
+        "46vw": "46.4vw",
       },
-      height:{
-        '46vw':'46.4vw'
-      }
+      height: {
+        "46vw": "46.4vw",
+      },
+      keyframes: {
+        fadeInDown: {
+          "0%": { opacity: "0", transform: "translateY(-100px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeInDown: "fadeInDown 3s 2s infinite",
+      },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+  plugins: [require("tailwind-scrollbar-hide")],
+};
