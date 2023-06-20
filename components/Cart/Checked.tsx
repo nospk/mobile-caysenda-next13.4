c4879bbbdba60f4dbb2b866bdc29633270af35db
nospk <nospk1706@gmail.com>
2023-06-20 07:36:44 +0000

    update cart

---
+++ components/Cart/Checked.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+const ActiveFull = () => {
+  return (
+    <div className={styles.checked_active_full}>
+      <span className={styles.checked_active_full_icon}></span>
+    </div>
+  );
+};
+const HaftFull = () => {
+  return (
+    <div className={styles.checked_active_half}>
+      <span className={styles.checked_active_half_icon}></span>
+    </div>
+  );
+};
+const NotFull = () => {
+  return <div className={styles.checked_not_active}></div>;
+};
+
+export { ActiveFull, HaftFull, NotFull };
