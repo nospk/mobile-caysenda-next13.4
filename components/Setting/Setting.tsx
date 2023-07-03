32673b9cb91d5aa6f709351f7a4e36bbad10d7de
khanhtrinh <khanhtrinh170899@gmail.com>
2023-07-03 16:21:03 +0700

    update_setting

--- components/Setting/Setting.tsx
+++ components/Setting/Setting.tsx
@@ -1,66 +1,83 @@
 
 'use client';
 import Link from 'next/link';
-import { FaEllipsisH } from 'react-icons/fa';
-
+import {
+  FaEllipsisH,
+  FaUserEdit,
+  FaCreativeCommonsNd,
+  FaRegLaugh,
+  FaRegEye
+} from 'react-icons/fa';
+import {
+  IoIosArrowBack,
+  IoIosArrowForward
+} from 'react-icons/io';
+import {FiLock } from 'react-icons/fi';
 const SettingPage = () => {
   return (
     <div className="min-h-screen bg-white">
-      <div className="flex justify-between items-center border-b-8 border-gray-300 h-16 py-4 px-8">
-
-        <h1 className="flex flex-1 items-center text-xl font-bold">
-          Cài đặt
-        </h1>
-
-        <div className="relative inline-block">
-          <FaEllipsisH size={18} />
-          <div className="bg-red-500 rounded-full flex items-center justify-center h-5 w-5 absolute -top-2 -right-3">
-            <p className="text-white text-sm font-medium leading-none">
-              2
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="max-w-md bg-white rounded-lg px-2">
+      <div className="bg-white rounded-lg px-2">
         <ul className="">
-          <li className="border-b border-gray-300 pt-4">
-            <Link href="/change-avatar" legacyBehavior>
-              <a className="block hover:text-red-500 mt-4 mb-4">Thay đổi Avatar</a>
+          <li className="border-b border-gray-300 py-4">
+            <Link href="/change-avatar">
+              <div className="flex">
+                <div className="pr-2 flex-none">
+                  <FaCreativeCommonsNd size={18} />
+                </div>
+                <div className="grow">Thay đổi Avatar</div>
+                <IoIosArrowForward size={18} className="flex-none" />
+              </div>
             </Link>
           </li>
-          <li className="border-b border-gray-300 pt-4">
-            <Link href="/" legacyBehavior>
-              <a className="block hover:text-red-500 mt-4 mb-4">
-                Tên Người Dùng
-              </a>
+          <li className="border-b border-gray-300 py-4">
+            <div className="flex">
+              <div className="pr-2 flex-none">
+                <FaRegLaugh size={18} />
+              </div>
+              <div className="px-1 grow ">Tên Đăng Nhập</div>
+              <div className="flex-none px-4">Nomi</div>
+            </div>
+          </li>
+          <li className="border-b border-gray-300 py-4">
+            <div className="flex">
+              <div className="pr-2 flex-none">
+                <FiLock size={18} />
+              </div>
+              <div className="px-1 grow ">Số Điện Thoại</div>
+              <div className="flex-none px-4">0123456789</div>
+            </div>
+          </li>
+          <li className="py-4">
+            <Link href="/change-password">
+              <div className="flex">
+                <div className="pr-2 flex-none">
+                  <FaRegEye size={18} />
+                </div>
+                <div className="grow">Đổi Mật Khẩu</div>
+                <IoIosArrowForward size={18} className="flex-none" />
+              </div>
             </Link>
           </li>
-          <li className="border-b border-gray-300 pt-4">
-            <Link href="/" legacyBehavior>
-              <a className="block hover:text-red-500 mt-4 mb-4">
-                Số Điện Thoại
-              </a>
-            </Link>
-          </li>
-          <li className="border-b border-gray-300 pt-4">
-            <Link href="/change-password" legacyBehavior>
-              <a className="block hover:text-red-500 mt-4 mb-4">Thay đổi Mật khẩu</a>
-            </Link>
-          </li>
-          <li className="border-b border-gray-300 pt-4">
-            <Link href="/shipping-address" legacyBehavior>
-              <a className="block hover:text-red-500 mt-4 mb-4">Địa chỉ giao hàng</a>
+          <li className="border-b border-gray-300 py-4">
+            <Link href="/change-address">
+              <div className="flex">
+                <div className="pr-2 flex-none">
+                  <FaUserEdit size={18} />
+                </div>
+                <div className="grow">Địa Chỉ Giao Hàng</div>
+                <IoIosArrowForward size={18} className="flex-none" />
+              </div>
             </Link>
           </li>
         </ul>
       </div>
       <div className="absolute inset-x-0 bottom-0 border-b border-gray-300 pt-4 pb-2 mb-0">
-            <Link href="/logout" legacyBehavior className="w-14">
-              <a className="block text-center text-red-500 hover:text-red-600 font-semibold mt-4 mb-4 text-xl">
-                Đăng xuất
-              </a>
-            </Link>
-        </div>
+        <Link href="/logout" className="w-14">
+          <div className="block text-center text-red-500 hover:text-red-600 font-semibold mt-4 mb-4 text-xl">
+            Đăng xuất
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
