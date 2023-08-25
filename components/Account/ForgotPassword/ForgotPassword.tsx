b196b0ff2b0e55e66200f34f156265676f020763
khanhtrinh <khanhtrinh170899@gmail.com>
2023-08-25 19:14:51 +0700

    swipe_button

--- components/Account/ForgotPassword/ForgotPassword.tsx
+++ components/Account/ForgotPassword/ForgotPassword.tsx
@@ -2,7 +2,7 @@
 import { FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation'
 import styles from './styles.module.css';
-import SwipeButton from 'react-swipezor';
+import SwipeButton from './SwipeButton/SwipeButton';
 import { IoIosArrowBack } from 'react-icons/io';
 import AccountService from '@/services/Account.service'
 
@@ -11,15 +11,17 @@ const ForgotPassword = () => {
     const [reset, setReset] = useState(0);
     const router = useRouter();
     const [email, setEmail] = useState('');
-    const [msg, setMsg] = useState('đã gửi email');
+    const [msg, setMsg] = useState('');
 
 
     async function handleSubmit(e: FormEvent<HTMLFormElement>) {
-        let res = await AccountService.getPasswordApi(email);
-        if (res.status == 200) {
-            const req = await res.json();
-            setMsg(req.msg)
-        }
+        // let res = await AccountService.getPasswordApi(email);
+        // if (res.status == 200) {
+        //     const req = await res.json();
+        //     setMsg(req.msg)
+        // }
+        setReset(e => e+1)
+        setMsg('đã kéo');
     }
     return (
         <div className={styles.container}>
@@ -41,15 +43,13 @@ const ForgotPassword = () => {
                             setEmail(e.target.value)
                         }} />
                 </div>
-                <div className={styles.btn_block}>
-                    <SwipeButton
-                        mainText="Kéo để lấy lại mật khẩu"
-                        overlayText="Kéo để lấy lại mật khẩu"
-                        classList={styles.swipe_block}
-                        onSwipeDone={handleSubmit}
-                        reset={reset}
-                    />
-                </div>
+                <SwipeButton
+                    mainText="Kéo để lấy lại mật khẩu"
+                    overlayText="Kéo để lấy lại mật khẩu"
+                    classList={styles.swipe_block}
+                    onSwipeDone={handleSubmit}
+                    reset={reset}
+                />
             </div>
             {msg && (
                 <div className={styles.msg_block}>
---
+++ components/Account/ForgotPassword/SwipeButton/SwipeButton.tsx
@@ -0,0 +1,118 @@
+'use client'
+import React, { useState, useEffect, useRef } from 'react'
+import { useSwipeable } from 'react-swipeable';
+import Arrow from "@/public/arrow.png"
+import Image from 'next/image'
+import styles from  "./swipezor.module.css";
+
+
+interface SwipezorProps {
+  mainText: any;
+  overlayText: any;
+  onSwipeDone: Function;
+  reset?: number;
+  classList?: string;
+  overlayClassList?: string;
+  caretClassList?: string;
+  delta?: number;
+  minSwipeWidth?: number;
+  minSwipeVelocity?: number;
+  caret?: any;
+}
+
+function findLeft(element: { getBoundingClientRect: () => any; } | undefined) {
+  if (element) {
+    var rec = element.getBoundingClientRect();
+  }
+  return rec.left + window.scrollX;
+}
+
+function SwipeButton({
+  mainText,
+  overlayText,
+  onSwipeDone,
+  reset,
+  classList = '',
+  overlayClassList = '',
+  caretClassList = '',
+  delta = 10,
+  minSwipeWidth = 0.6,
+  minSwipeVelocity = 0.6,
+  caret = null
+}: SwipezorProps) {
+  const [overlayWidth, setOverlayWidth] = useState(40);
+  const [swipeComplete, setSwipeComplete] = useState(false);
+  const buttonRef = useRef<HTMLButtonElement>();
+
+  useEffect(() => {
+    if (reset) {
+      setSwipeComplete(false);
+      setOverlayWidth(40);
+    }
+  }, [reset]);
+
+  const handlers = useSwipeable({
+    onSwipedRight: (data) => {
+      if (swipeComplete)
+        return;
+
+      const butWidth = buttonRef.current ? buttonRef.current.offsetWidth : 50;
+
+      if (data.velocity && data.velocity > minSwipeVelocity) {
+        setOverlayWidth(butWidth);
+        setSwipeComplete(true);
+        setTimeout(() => onSwipeDone(), 100);
+      }
+      else {
+        const offsetLeft = findLeft(buttonRef.current);
+        const startPos = Math.abs(data.initial[0] - offsetLeft);
+        if (startPos <= 100 &&
+          (data.event instanceof TouchEvent ?  data.event.changedTouches[0].clientX - offsetLeft : data.event.offsetX) > minSwipeWidth * butWidth
+        ) {
+          setOverlayWidth(butWidth);
+          setSwipeComplete(true);
+          setTimeout(() => onSwipeDone(), 100);
+        }
+        else
+          setOverlayWidth(40);
+      }
+    },
+    onSwiping: (data) => {
+      if (swipeComplete)
+        return;
+      const offsetLeft = findLeft(buttonRef.current);
+      const startPos = Math.abs(data.initial[0] - offsetLeft);
+      if (startPos <= 100) {
+        if (data.event && data.event instanceof TouchEvent )
+          setOverlayWidth(data.event.touches[0].clientX - offsetLeft);
+        else
+          setOverlayWidth(data.event.offsetX);
+      }
+
+    },
+    delta,
+    trackMouse: true,
+    preventDefaultTouchmoveEvent: true
+  });
+
+  return (
+    <div className={`${styles.swipezor_but} ${classList}`} {...handlers} ref={(t) => {
+      handlers.ref(t);
+      buttonRef.current = t as unknown as HTMLButtonElement;
+    }}>
+      <div className={`${styles.swipezor_overlay} ${overlayClassList}`} style={{ width: overlayWidth }}>
+        <div className={styles.swipezor_overlay_wrapper}>
+          <div className={`${styles.swipezor_caret_wrapper} ${caretClassList}`}>
+            {caret ? caret : <Image src={Arrow} alt="caret" style={{ maxWidth: '100%' }} />}
+          </div>
+          <div className={styles.swipezor_overlay_txt}>
+            {overlayText}
+          </div>
+        </div>
+      </div>
+      {mainText}
+    </div>
+  )
+}
+
+export default SwipeButton;
