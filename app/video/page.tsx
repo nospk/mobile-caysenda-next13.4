73b1b2cbd3a82c89268be607d171d35348ebed2b
nospk <nospk1706@gmail.com>
2023-09-12 22:28:10 +0700

    update

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,53 +1,95 @@
 import VideoPlayer from "@/components/VideoPlayer";
 import { BsFire } from "react-icons/bs";
-import { AiFillCarryOut } from "react-icons/ai";
+import { AiFillCarryOut, AiOutlineLeft } from "react-icons/ai";
 import Image from "next/image";
+import ProductService from "@/services/Product.service";
+import { convertMoney } from "@/lib/formatPrice";
+import WholeSaleModal from "@/components/AddToCartModal/WholeSaleModal";
+import RetailModal from "@/components/AddToCartModal/RetailModal";
+import Link from "next/link";
+import styles from "./styles.module.css";
+interface Variant {
+  id: string;
+  price: string;
+  image: string;
+}
+
+interface ProductDetail {
+  id: number;
+  slug: string;
+  name: string;
+  unit: string;
+  variants: Variant[];
+  retail: boolean;
+  sold: number;
+  category_slug: string;
+}
 export default async function Page({ searchParams }: any) {
   const id: string = searchParams.id;
-
+  //Will edit by search ProductId
+  const slug = "ech-da-quang-zc-559";
+  const data: ProductDetail = await ProductService.getDetail({
+    slug: slug,
+  });
   return (
     <>
+      <Link href={`/videos`}>
+        <div className={styles.back}>
+          <AiOutlineLeft className={styles.icon_back} />
+        </div>
+      </Link>
       <VideoPlayer id={id} />
-      <div className="box-broder absolute bottom-0 flex w-[100vw] flex-shrink-0 flex-col">
-        <div className="relative mb-[9.0667vw] box-border flex flex-shrink-0 flex-col pl-[3.2vw]">
-          <div className="relative box-border flex h-[14.9333vw] flex-shrink-0 flex-row place-content-between pr-[3.2vw]">
-            <div className="relative box-border block flex-shrink-0 flex-col content-start leading-[3.4667vw] text-white">
-              <div className="text-[4.2vw] ">Chậu Hình Thú</div>
-              <div className="mt-[2.6667vw] flex flex-row items-center justify-start text-[2.7vw] text-gray-400">
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.info}>
+            <div className={styles.info_wrapper}>
+              <div className={styles.name}>{data.name}</div>
+              <div className={styles.sold}>
                 <BsFire size={16} />
-                <span className="leading-[16px]">14 Cái Đã Bán</span>
+                <span className={styles.sold_number}>
+                  {data.sold} {data.unit} Đã Bán
+                </span>
               </div>
             </div>
           </div>
-          <div className="relative box-border flex h-[26.1333vw] w-full flex-shrink-0 flex-col content-start overflow-hidden">
-            <div className="relative flex h-[29.3333vw] w-full flex-shrink-0 flex-row content-start overflow-scroll pr-[2.6667vw]">
-              <div className="relative flex flex-shrink-0 flex-row content-start">
-                <div className="relative mr-[2.6667vw] box-border flex h-[26.1333vw] w-[20.8vw] flex-shrink-0 flex-col content-start items-center rounded-lg bg-white pt-[1.0667vw]">
-                  <div className="relative box-border flex h-[18.6667vw] w-[18.6667vw] flex-shrink-0 flex-col content-start overflow-hidden">
-                    <Image
-                      className="rounded-lg"
-                      src="https://caysenda.vn/resources/upload/1920487345_394298364.jpg"
-                      alt="a"
-                      sizes="100vw"
-                      width={0}
-                      height={0}
-                      style={{ width: "100%", height: "100%" }}
-                    />
+          <div className={styles.list_item}>
+            <div className={styles.list_item_wrapper}>
+              <div className={styles.variants}>
+                {data.variants.map((variant: any) => (
+                  <div key={variant.id} className={styles.variant_content}>
+                    <Link href={`/${data.category_slug}/${slug}`}>
+                      <div className={styles.variant_image}>
+                        <Image
+                          className="rounded-lg"
+                          src={variant.thumbnail}
+                          alt={variant.name}
+                          sizes="100vw"
+                          width={0}
+                          height={0}
+                          style={{ width: "100%", height: "100%" }}
+                        />
+                      </div>
+                      <span className={styles.variant_price}>
+                        {convertMoney(variant.price)}đ
+                      </span>
+                    </Link>
                   </div>
-                  <span className="relative mt-[0.9333vw] box-border block text-sm leading-[4vw] text-red-500">
-                    9.000đ
-                  </span>
-                </div>
+                ))}
               </div>
             </div>
           </div>
-          <div className="relative mt-[3.2vw] box-border flex flex-shrink-0 flex-row place-content-between text-white">
-            <div className="relative">
+          <div className={styles.add_to_cart}>
+            {/* <div className="relative">
               <AiFillCarryOut size={24} />
               <div className="absolute -top-2 left-[14px] h-[20px] min-w-[30px] rounded-3xl border border-white bg-[red] text-sm">
-                <div className="text-center leading-[20px]">2</div>
+                <div className="text-center leading-[20px]">{data.variants.length}</div>
               </div>
-            </div>
+            </div> */}
+            {data.retail ? (
+              <RetailModal className={styles.button} productId={data.id} />
+            ) : (
+              <WholeSaleModal className={styles.button} productId={data.id} />
+            )}
           </div>
         </div>
       </div>
