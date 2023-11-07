b75eb65a250d09fbfd05f19836c06625bc8f23f2
nospk <nospk1706@gmail.com>
2023-11-07 18:59:09 +0700

    update

--- app/(setting)/change-address/page.tsx
+++ app/(setting)/change-address/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
-function changeAddress() {
+"use client";
+import React, { useEffect, useState } from "react";
+import { ListDelivery } from "@/types/Delivery";
+import AddressService from "@/services/Address.service";
+function ChangeAddress() {
+  const [listDelivery, setListDelvery] = useState<ListDelivery[]>([]);
+  useEffect(() => {
+    const setup = async () => {
+      const getdata = await AddressService.getListDelivery();
+      setListDelvery(getdata);
+    };
+    setup();
+  }, []);
   return (
-    <div>changeAddress</div>
-  )
+    <>
+      {listDelivery.map((item, index) => (
+        <div key={index}>
+          <span>{item.address}</span>;
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default changeAddress
+export default ChangeAddress;
